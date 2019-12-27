0ff2cc2b4684594c935835a6cf035375e577e38e
= <danb.alexandru@gmail.com>
2019-12-27 16:30:07 +0200

    [fix]   - the 'main.py' file was not commited properly in the '3ec90cabca46644f10a3217524a6c5db2664a0e7' commit   - this commit is meant to import the latest (functioning) version of   this file

--- main.py
+++ main.py
@@ -4,10 +4,25 @@ Created on Fri Dec 27 14:09:17 2019
 
 @author: Dan Alexandru-Bogdan
 """
+# %% Imports
+from globals import console
+from get_csv_files import get_csv_files
 
-#%% Main
+
+# %% Main
 def main():
-    print('Hello World')
+    """
+    The function is the main section from which the project is being run
 
-if __name__ == 'main':
+    :returns: Boolean (True or False)
+    """
+    try:
+        get_csv_files.run()
+        return True
+    except Exception as error_message:
+        console.log(error_message, console.LOG_ERROR)
+        return FileExistsError
+
+
+if __name__ == '__main__':
     main()
