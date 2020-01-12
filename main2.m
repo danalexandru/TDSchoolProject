f7bd722d2d1138abf45c5c9a9c02e8bde0c0e8e8
= <danb.alexandru@gmail.com>
2020-01-12 19:00:15 +0200

    [update]   - updated the 'main2.m' script in order to iterate throwgh all the   datasets from the 'datasets.mat' file

--- main2.m
+++ main2.m
@@ -9,27 +9,75 @@ clear;
 [healthy_data, broken_data] = get_all_datasets();
 
 %% Get the medium frequencies
-dataset = healthy_data(1).files;
-mean_freq = get_medium_frequencies(dataset);
+dict_healthy_mean_freq = containers.Map;
+dict_broken_mean_freq = containers.Map;
+
+for i = 1 : length(healthy_data)
+    dict_healthy_mean_freq(healthy_data(i).files.name) = get_medium_frequencies(healthy_data(i).files);
+end
+
+for i = 1 : length(broken_data)
+    dict_broken_mean_freq(broken_data(i).files.name) = get_medium_frequencies(broken_data(i).files);
+end
 
 %% Get the frequency magnitude
-dataset = healthy_data(1).files;
-freq_magn = get_frequencies_magnitude(dataset);
+dict_healthy_freq_magn = containers.Map;
+dict_broken_freq_magn = containers.Map;
+
+for i = 1 : length(healthy_data)
+    dict_healthy_freq_magn(healthy_data(i).files.name) = get_frequencies_magnitude(healthy_data(i).files);
+end
+
+for i = 1 : length(broken_data)
+    dict_broken_freq_magn(broken_data(i).files.name) = get_frequencies_magnitude(broken_data(i).files);
+end
 
 %% Get the kurtosis for each sensor
-dataset = healthy_data(1).files;
-kustosis_measure = get_kurtosis(dataset);
+dict_healthy_kustosis_measure = containers.Map;
+dict_broken_kustosis_measure = containers.Map;
+
+for i = 1 : length(healthy_data)
+    dict_healthy_kustosis_measure(healthy_data(i).files.name) = get_kurtosis(healthy_data(i).files);
+end
+
+for i = 1 : length(broken_data)
+    dict_broken_kustosis_measure(broken_data(i).files.name) = get_kurtosis(broken_data(i).files);
+end
 
 %% Get the skewness for each sensor
-dataset = healthy_data(1).files;
-skewness_measure = get_skewness(dataset);
+dict_healthy_skewness_measure = containers.Map;
+dict_broken_skewness_measure = containers.Map;
+
+for i = 1 : length(healthy_data)
+    dict_healthy_skewness_measure(healthy_data(i).files.name) = get_skewness(healthy_data(i).files);
+end
+
+for i = 1 : length(broken_data)
+    dict_broken_skewness_measure(broken_data(i).files.name) = get_skewness(broken_data(i).files);
+end
 
 %% Get the entropy of the sensors
-dataset = healthy_data(1).files;
-signal_entropy = get_signal_entropy(dataset);
+dict_healthy_signal_entropy = containers.Map;
+dict_broken_signal_entropy = containers.Map;
+
+for i = 1 : length(healthy_data)
+    dict_healthy_signal_entropy(healthy_data(i).files.name) = get_signal_entropy(healthy_data(i).files);
+end
+
+for i = 1 : length(broken_data)
+    dict_broken_signal_entropy(broken_data(i).files.name) = get_signal_entropy(broken_data(i).files);
+end
 
 %% Get the entropy of the sensors
-dataset = healthy_data(1).files;
-iqr_data = get_iqr(dataset);
+dict_healthy_iqr_data = containers.Map;
+dict_broken_iqr_data = containers.Map;
+
+for i = 1 : length(healthy_data)
+    dict_healthy_iqr_data(healthy_data(i).files.name) = get_iqr(healthy_data(i).files);
+end
+
+for i = 1 : length(broken_data)
+    dict_broken_iqr_data(broken_data(i).files.name) = get_iqr(broken_data(i).files);
+end
 
 end
