74b5a842b2d4defd0cebb9981d240176f3bae874
= <danb.alexandru@gmail.com>
2020-01-12 19:08:26 +0200

    [add]   - added the save as mat functionality for all the matlab functions   - added the 'dict_*.mat' files in order for them to be used in python

--- main2.m
+++ main2.m
@@ -20,6 +20,9 @@ for i = 1 : length(broken_data)
     dict_broken_mean_freq(broken_data(i).files.name) = get_medium_frequencies(broken_data(i).files);
 end
 
+save('dict_healthy_mean_freq.mat', 'dict_healthy_mean_freq');
+save('dict_broken_mean_freq.mat', 'dict_broken_mean_freq');
+
 %% Get the frequency magnitude
 dict_healthy_freq_magn = containers.Map;
 dict_broken_freq_magn = containers.Map;
@@ -31,6 +34,9 @@ end
 for i = 1 : length(broken_data)
     dict_broken_freq_magn(broken_data(i).files.name) = get_frequencies_magnitude(broken_data(i).files);
 end
+
+save('dict_healthy_freq_magn.mat', 'dict_healthy_freq_magn');
+save('dict_broken_freq_magn.mat', 'dict_broken_freq_magn');
 
 %% Get the kurtosis for each sensor
 dict_healthy_kustosis_measure = containers.Map;
@@ -44,6 +50,9 @@ for i = 1 : length(broken_data)
     dict_broken_kustosis_measure(broken_data(i).files.name) = get_kurtosis(broken_data(i).files);
 end
 
+save('dict_healthy_kustosis_measure.mat', 'dict_healthy_kustosis_measure');
+save('dict_broken_kustosis_measure.mat', 'dict_broken_kustosis_measure');
+
 %% Get the skewness for each sensor
 dict_healthy_skewness_measure = containers.Map;
 dict_broken_skewness_measure = containers.Map;
@@ -55,6 +64,9 @@ end
 for i = 1 : length(broken_data)
     dict_broken_skewness_measure(broken_data(i).files.name) = get_skewness(broken_data(i).files);
 end
+
+save('dict_healthy_skewness_measure.mat', 'dict_healthy_skewness_measure');
+save('dict_broken_skewness_measure.mat', 'dict_broken_skewness_measure');
 
 %% Get the entropy of the sensors
 dict_healthy_signal_entropy = containers.Map;
@@ -68,6 +80,9 @@ for i = 1 : length(broken_data)
     dict_broken_signal_entropy(broken_data(i).files.name) = get_signal_entropy(broken_data(i).files);
 end
 
+save('dict_healthy_signal_entropy.mat', 'dict_healthy_signal_entropy');
+save('dict_broken_signal_entropy.mat', 'dict_broken_signal_entropy');
+
 %% Get the entropy of the sensors
 dict_healthy_iqr_data = containers.Map;
 dict_broken_iqr_data = containers.Map;
@@ -79,5 +94,8 @@ end
 for i = 1 : length(broken_data)
     dict_broken_iqr_data(broken_data(i).files.name) = get_iqr(broken_data(i).files);
 end
+
+save('dict_healthy_iqr_data.mat', 'dict_healthy_iqr_data');
+save('dict_broken_iqr_data.mat', 'dict_broken_iqr_data');
 
 end
